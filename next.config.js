c337bfa563b9e981e25528400235b108dd277f17
ivfarias <imivanfarias@gmail.com>
2024-02-29 17:08:42 -0300

    blok party

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    i18n: {
+        locales: ['en', 'es'],
+        defaultLocale: 'en',
+    },
+    env: {
+        storyblokApiToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
+    },
+}
